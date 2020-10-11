7ffe40b24348cbabeae36b397b12d9146b993846
MunchingFruit <53832997+MunchingFruit@users.noreply.github.com>
2020-10-11 13:39:01 +0200

    Put all required files

---
+++ src/Server.java
@@ -0,0 +1,39 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private static void run(){
+
+        try(ServerSocket server = new ServerSocket(34522);) {
+            while(true) {
+                try (
+                        Socket socket = server.accept();
+                        DataInputStream input = new DataInputStream(socket.getInputStream());
+                        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                ) {
+                    String msg = input.readUTF();
+                    System.out.println(msg);
+                }
+            }
+
+                } catch (IOException e) {
+                    System.out.println("IO EXPECTION HAHA");
+                }
+        }
+
+
+
+
+
+    public static void main(String[] args) {
+
+        run();
+
+    }
+
+
+}
